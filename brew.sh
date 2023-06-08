5644cd62948c9fa23c1b09afac9311431b57a3a5
Chris Van Dyke <cpv0310@live.com>
2023-06-08 09:09:12 -0500

    brew update

--- brew.sh
+++ brew.sh
@@ -103,3 +103,103 @@ brew install exa
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+
+##look at these
+
+# Install command-line tools using Homebrew
+
+# (Optionally) Turn off brew's analytics https://docs.brew.sh/Analytics
+# brew analytics off
+
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# GNU `sed`
+brew install gnu-sed
+
+
+# Updated shells
+# Note: don’t forget to add `/usr/local/bin/<EACHSHELL>` to `/etc/shells` before running `chsh`.
+brew install fish
+
+brew install bash-completion
+
+
+# Install wget
+brew install wget
+
+# Install more recent versions of some OS X tools
+brew install vim
+brew install nano
+brew install grep
+
+# z hopping around folders
+brew install z
+
+# run this script when this file changes guy.
+brew install entr
+
+# github util
+brew install gh
+# nicer git diffs
+brew install git-delta
+
+# better `top`
+brew install glances
+
+brew install shellcheck # linting for .sh files
+
+
+# mtr - ping & traceroute. best.
+brew install mtr
+
+    # allow mtr to run without sudo
+    # https://github.com/traviscross/mtr/issues/204#issuecomment-487325796
+    sudo chmod 4755 $location_of_mtr-packet
+    # on my machine i have a `/usr/local/sbin/mtr-packet` and root owns local/sbin. (thx google!?)
+    # thus, i dont use the homebrew mtr.
+
+
+
+# Install other useful binaries
+brew install the_silver_searcher # ack is an alternative, tbh i forget which i like more.
+brew install fzf
+
+brew install imagemagick
+brew install node # This installs `npm` too using the recommended installation method
+brew install rename
+brew install tree
+brew install ffmpeg
+
+# json stuff
+brew install jq gron
+
+brew install ncdu # find where your diskspace went
+
+
+brew install scrcpy # control/view android phone from PC. amazing
+brew install youtube-dl
+
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install spotify
+brew cask install discord
+brew cask install google-backup-and-sync
+brew cask install skype
+brew cask install gimp
+brew cask install vlc
+brew cask install hyperdock
+brew cask install divvy
+
+brew install --cask iterm2
+brew install --cask sublime-text
+brew install --cask imageoptim
